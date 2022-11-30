27b565adb318a56f1e3259503efd4faee398a4a5
blurp748 <mathistanghe@hotmail.fr>
2022-11-30 09:52:05 +0100

    inversement

--- front-svelte/src/components/Range.svelte
+++ front-svelte/src/components/Range.svelte
@@ -4,10 +4,10 @@
 
 <div class="grid items-end">
     <div>
-        <input type="range" name="price" min="0" max="100" value={neutrality} class="w-screen h-2 bg-gradient-to-r from-blue-600 to-red-600 appearance-none" disabled />
+        <input type="range" name="price" min="0" max="100" value={neutrality} class="w-screen h-2 bg-gradient-to-r from-red-600 to-blue-600 appearance-none" disabled />
       </div>
     <div class="flex justify-between">
-        <p class="ml-2 text-white">Gentillesse</p>
-        <p class="mr-2 text-white">Méchanceté</p>
+        <p class="ml-2 text-white">Méchanceté</p>
+        <p class="mr-2 text-white">Gentillesse</p>
     </div>
 </div>
